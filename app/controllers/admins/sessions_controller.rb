3c4d4951cacb90bbef0c4c54844bc9e1df71a945
yuri0427a <yuri0427ita@gmail.com>
2020-02-25 11:48:09 +0000

    [Finish]admin会員詳細画面view

--- app/controllers/admins/sessions_controller.rb
+++ app/controllers/admins/sessions_controller.rb
@@ -18,10 +18,16 @@ class Admins::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  def after_sign_in_path_for(resource) 
+    admins_admins_homes_top
+  end
+
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+  def configure_sign_in_params
+     devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :passwar
+  end
+
+  
 end
