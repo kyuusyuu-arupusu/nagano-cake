6b52c72f0ce107d9a7ef9291a46297dcc6122ae2
takumitakeno <matchball.shine@gmail.com>
2020-02-26 03:05:45 +0000

    customers_show_edit

--- app/controllers/customers/customers_controller.rb
+++ app/controllers/customers/customers_controller.rb
@@ -15,14 +15,14 @@ class Customers::CustomersController < ApplicationController
   end
 
   def update
-     　customer = Customer.find(params[:id])
-     　customer.update(customer_params)
-     　redirect_to root_path
+      customer = Customer.find(params[:id])
+      customer.update(customer_params)
+      redirect_to customers_customer_path
   end
 
   private
   def  customer_params
-        params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :post_code, :addres, :phone_namuber, :email)
+        params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :post_code, :address, :phone_namber, :email)
 
     end
 end
